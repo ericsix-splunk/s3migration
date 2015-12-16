a8815c443741bf0186faadc006fe980e54bde8d9
ericsix-splunk <esix@splunk.com>
2015-12-16 11:31:40 -0800

    Added rsync function
    Added local rsync function for after buckets are copied

--- splunks3move.sh
+++ splunks3move.sh
@@ -11,7 +11,7 @@ LogFile=/tmp/BucketCopy.log
 S3CMD=`which s3cmd`
 
 
-options=("backup to s3 bucket" "restore to s3 bucket")
+options=("backup to s3 bucket" "restore to s3 bucket" "move buckets to splunkDB")
 PS3="Chose your options  : "
 
 # backup to s3 function. s3cmd doesnt return error codes other then 0
@@ -29,13 +29,13 @@ function s3backup {
     echo " "
     	$S3CMD info s3://$S3BucketLocation 2>&1 | grep ERROR
 	if [ $? -eq 1 ]; then
-	 read -p "Bucket Exists, ready to copy. Press [Enter]..." 
-	else 
+	 read -p "Bucket Exists, ready to copy. Press [Enter]..."
+	else
           echo " ERROR with $S3BucketLocation "
 	  read -p "Bucket doesnt exist.. please check again and confirm full path. Press [Enter]"
 	  s3backup
 	fi
-    wait 
+    wait
     clear
     echo " "
     echo " From here, sync of all indexes will begin. "
@@ -45,9 +45,11 @@ function s3backup {
     echo " "
     echo "Starting copy, this will take time..... "
     echo "Starting S3 Bucket Copy to $S3BucketLocation" | tee -a $LogFile
-    echo "Using s3cmd : s3cmd sync --dry-run --rexclude=\"(_internaldb|persistentstorage|fishbucket|sos|history|sos_summary_daily|splunklogger|summary|_introspectiondb|_introspection)\/.*\""
+    echo "Using s3cmd : s3cmd sync --dry-run --rexclude=\"(_internaldb|persistentstorage|fishbucket|sos|history|sos_summary_daily|splunklogger|summary|_introspectiondb|_introspection)
+\/.*\""
     echo "  --rexclude=\"\/db\/hot.*\" --rexclude=\"\/datamodel_summary\/.*\" /opt/splunk/var/lib/spunk/ s3://$S3BucketLocation"
-    $S3CMD sync --rexclude="(_internaldb|persistentstorage|fishbucket|sos|history|sos_summary_daily|splunklogger|summary|_introspectiondb|_introspection)\/.*" --rexclude="\/db\/hot.*" --rexclude="\/datamodel_summary\/.*" $SplunkDBLocation s3://$S3BucketLocation | tee -a $LogFile
+    $S3CMD sync --rexclude="(_internaldb|persistentstorage|fishbucket|sos|history|sos_summary_daily|splunklogger|summary|_introspectiondb|_introspection)\/.*" --rexclude="\/db\/hot.*"
+ --rexclude="\/datamodel_summary\/.*" $SplunkDBLocation s3://$S3BucketLocation | tee -a $LogFile
     echo "Copy finished. " | tee -a $LogFile
     break
 }
@@ -75,21 +77,46 @@ function s3restore {
     clear
     echo " "
     echo " From here, sync of all indexes will begin. "
-    echo " These will be copied to : $SplunkBucketImportPath." 
+    echo " These will be copied to : $SplunkBucketImportPath."
     echo " "
-    echo "Starting copy, this will take time....." 
+    echo "Starting copy, this will take time....."
     echo "Starting S3 Bucket Copy from $S3BucketLocation to $SplunkBucketImportPath." | tee -a $LogFile
     $S3CMD sync s3://$S3BucketLocation $SplunkBucketImportPath | tee -a $LogFile
     echo "Copy finished. " | tee -a $LogFile
     break
 }
 
+# move migrated file to splunkDB directory
+function movetosplunkDB {
+    local SplunkDBPath=/opt/splunk/var/lib/splunk
+    local RSYNC=`which rsync`
+    clear
+    echo " Index Bucket Restore Function" | tee -a $LogFile
+    echo " This will restore all warm / cold buckets to splunk_home/var/lib/splunk/. After this is done, the " | tee -a $LogFile
+    echo " indexer should be restarted and validate the buckets are readable." |  tee -a $LogFile
+    echo ".................................................................................." |  tee -a $LogFile
+    echo " **** Note these are single instance buckets in a Clustered Environment. These buckets" | tee -a $LogFile
+    echo " **** will  not be replicated. " | tee -a $LogFile
+    echo ".................................................................................." | tee -a $LogFile
+    echo " This will copy from $SplunkBucketImportPath to $SplunkDBPath." | tee -a $LogFile
+    echo "" | tee -a $LogFile
+    wait
+    echo "Starting local rsync to splunkdb path. " | tee -a $LogFile
+    echo " SRC : $SplunkBucketImportPath    DST : $SplunkDBPath." | tee -a $LogFile
+    echo " using : rsync --ignore-existing --update --dry-run $SplunkBucketImportPath $SplunkDBPath"
+    $RSYNC -rvPS --update $SplunkBucketImportPath $SplunkDBPath | tee -a $LogFile
+    echo ".... rsync finished. " | tee -a $LogFile
+    break
+}
+
+
+
 # lets make a logfile, with a timestamp.. perhaps we can splunk it
 function createLog {
    local current_time=$(date "+%Y/%m/%d %H:%M:%S")
-   echo "$current_time" > $LogFile 
-   echo "Bucket Copy to S3 Log" >> $LogFile
-   echo " " >> $LogFile
+   echo "$current_time" | tee  $LogFile
+   echo "Bucket Operations Log"  | tee -a $LogFile
+   echo " " | tee -a $LogFile
 }
 
 
@@ -112,6 +139,10 @@ do
         s3restore
         break
         ;;
+	"move buckets to splunkDB")
+	movetosplunkDB
+	break
+	;;
         "quit")
         echo "exiting...."
         break
@@ -119,6 +150,5 @@ do
         *) echo "invalid---";;
     esac
 done
-
 
 
